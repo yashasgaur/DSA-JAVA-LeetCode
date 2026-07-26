4d2bad40df1ad08ad0e54fe35945747c63cd6936
Yashas Gaur <yashasgaur@gmail.com>
2026-07-26 22:59:03 +0530

    Time: 67 ms (35.55%) | Memory: 47.4 MB (60.69%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -5,36 +5,36 @@ class Solution {
             sum += num;
         }
 
-        if(sum % 2 == 0){
-            int len = nums.length;
-            int half = sum / 2;
+        if(sum % 2 != 0)
+            return false;
 
-            boolean[][] dp = new boolean[len + 1][half + 1];
+        int len = nums.length;
+        int half = sum / 2;
 
-            for(int n = 0; n <= len; n++){
-                for(int bag = 0; bag <= half; bag++){
-                    if(n == 0){
-                        dp[n][bag] = false;
-                    }
-                    if(bag == 0){
-                        dp[n][bag] = true;
-                    }
+        boolean[][] dp = new boolean[len + 1][half + 1];
+
+        for(int n = 0; n <= len; n++){
+            for(int bag = 0; bag <= half; bag++){
+                if(n == 0){
+                    dp[n][bag] = false;
+                }
+                if(bag == 0){
+                    dp[n][bag] = true;
                 }
             }
-
-            for(int n = 1; n <= len; n++){
-                for(int bag = 1; bag <= half; bag++){
-                    if(nums[n - 1] <= bag){
-                        dp[n][bag] = 
-                        dp[n - 1][bag - nums[n - 1]] || dp[n - 1][bag];
-                    }
-                    else{
-                        dp[n][bag] = dp[n - 1][bag];
-                    }
-                }
-            }
-            return dp[len][half];
         }
-        return false;
+
+        for(int n = 1; n <= len; n++){
+            for(int bag = 1; bag <= half; bag++){
+                if(nums[n - 1] <= bag){
+                    dp[n][bag] = 
+                    dp[n - 1][bag - nums[n - 1]] || dp[n - 1][bag];
+                }
+                else{
+                    dp[n][bag] = dp[n - 1][bag];
+                }
+            }
+        }
+        return dp[len][half];
     }
 }
