6832b3ed3acdc9b035a67190e149b609c1af5bd3
Yashas Gaur <yashasgaur@gmail.com>
2026-08-08 16:06:37 +0530

    Time: 706 ms (19.24%) | Memory: 0B (100.00%) - LeetSync

---
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -0,0 +1,13 @@
+SELECT name AS Customers
+FROM Customers AS a
+LEFT JOIN Orders AS b
+ON a.id = b.customerId
+WHERE b.customerId IS NULL
+
+UNION ALL
+
+SELECT name AS Customers
+FROM Customers AS a
+RIGHT JOIN Orders AS b
+ON a.id = b.customerId
+WHERE a.id IS NULL
